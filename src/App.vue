ba3c7a7c852aba430e0f2e2ed23ecdb25611c3a9
Jocelino Neto <netoj96@live.com>
2022-11-15 21:42:07 -0300

    contact and experiences card extracted

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script lang="ts">
 import VButton from './components/Button.vue'
 import VHeader from './components/Header.vue'
+import VCardItem from './components/CardItem.vue'
 
 export default {
     components: {
         VButton,
+        VCardItem,
         VHeader,
     },
     methods: {
         openLinkedIn() {
             const url = 'https://www.linkedin.com/in/netojocelino'
-            console.log(url)
             window.open(url, '_blank')
         },
         openGitHub() {
             const url = 'https://github.com/netojocelino'
-            console.log(url)
             window.open(url, '_blank')
         },
     },
@@ -53,27 +53,22 @@ export default {
                 <div className="Description-Card">
                     <h2 className="Card-Title">Professional Contact</h2>
 
-                    <div className="Card-Body">
-                        <span className="icon">@</span>
-                        <div className="details">
-                            <span>email</span>
-                            <span>netoj96@live.com</span>
-                        </div>
-                    </div>
+                    <v-card-item
+                        icon="@"
+                        item-key="email"
+                        item-value="netoj96@live.com"
+                    />
                 </div>
 
                 <div className="Description-Card">
                     <h2 className="Card-Title">Experiences</h2>
 
-                    <dl className="Card-Body">
-                        <div className="details">
-                            <a className="details-title" href="http://githubx.com/netojocelino/?repository" target="_blank">
-                                Company Name
-                            </a>
-                            <span className="details-subtitle">City, State &mdash; Start Year - Final Year</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        </div>
-                    </dl>
+                    <v-card-item
+                        details-title="Company Name"
+                        details-anchor="http://githubx.com/netojocelino/?repository"
+                        details-subtitle="City, State &mdash; Start Year - Final Year"
+                        details-paragraph="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+                    />
                 </div>
             </div>
             <div className="Side-Menu">
@@ -134,34 +129,7 @@ export default {
     font-weight: normal;
 }
 
-.Description-Card .Card-Body {
-    display: flex;
-    flex-direction: row;
-}
 
-.Description-Card .Card-Body .icon {
-    font-size: 2.5rem;
-    user-select: none;
-}
-
-.Description-Card .Card-Body .details {
-    display: flex;
-    flex-direction: column;
-    margin: 0.5rem 0 0 0.5rem;
-}
-
-.Description-Card .Card-Body .details .details-title {
-    font-size: 1.2rem;
-}
-.Description-Card .Card-Body .details .details-subtitle {
-    font-size: 1rem;
-    color: var(--jt-main-gray);
-}
-
-.Description-Card .Card-Body .details p {
-    padding-top: .5rem;
-    padding-left: .5rem;
-}
 
 .Main-Content > .Side-Menu {
     display: flex;
---
+++ src/components/CardItem.vue
@@ -0,0 +1,126 @@
+<script lang="ts">
+export default {
+    computed: {
+        hasIcon() {
+            const isUndef = this.icon === undefined
+            const isNil = this.icon === null
+            const isString = typeof this.icon === 'string'
+
+            if (isUndef || isNil || !isString) {
+                return false
+            }
+
+            const isFill = this.icon!.length > 0
+
+            return !isUndef && !isNil && isFill
+        },
+        isAlternative() {
+            if (this.hasIcon) {
+                return false
+            }
+
+            const hasTitle = this.$props.detailsTitle !== undefined
+            const hasAnchor = this.$props.detailsAnchor !== undefined
+            const hasSubtitle = this.$props.detailsSubtitle !== undefined
+            const hasParagraph = this.$props.detailsParagraph !== undefined
+
+            return (hasAnchor && hasTitle) || hasSubtitle || hasParagraph
+        },
+    },
+    props: {
+        icon: {
+            type: String,
+            required: false,
+        },
+        itemKey: {
+            type: String,
+            required: false,
+        },
+        itemValue: {
+            type: String,
+            required: false,
+        },
+        detailsTitle: {
+            type: String,
+            required: false,
+        },
+        detailsAnchor: {
+            type: String,
+            required: false,
+        },
+        detailsSubtitle: {
+            type: String,
+            required: false,
+        },
+        detailsParagraph: {
+            type: String,
+            required: false,
+        },
+    },
+}
+</script>
+
+<template>
+<div className="Card-Body">
+    <template v-if="!isAlternative">
+        <span
+            v-if="hasIcon"
+            className="icon"
+        >{{ icon }}</span>
+    
+        <div
+            v-if="itemKey !== undefined && itemValue !== undefined"
+            className="details"
+        >
+            <span>{{ itemKey }}</span>
+            <span>{{ itemValue }}</span>
+        </div>
+    </template>
+    <template v-else>
+        <div className="details">
+            <a
+                className="details-title"
+                :href="detailsAnchor"
+                target="_blank"
+            >
+                {{ detailsTitle }}
+            </a>
+            <span className="details-subtitle">{{ detailsSubtitle }}</span>
+            <p>{{ detailsParagraph }}</p>
+        </div>
+    </template>
+</div>
+</template>
+
+
+<style scoped>
+.Card-Body {
+    display: flex;
+    flex-direction: row;
+}
+
+.Card-Body .icon {
+    font-size: 2.5rem;
+    user-select: none;
+}
+
+.Card-Body .details {
+    display: flex;
+    flex-direction: column;
+    margin: 0.5rem 0 0 0.5rem;
+}
+
+.Card-Body .details .details-title {
+    font-size: 1.2rem;
+}
+.Card-Body .details .details-subtitle {
+    font-size: 1rem;
+    color: var(--jt-main-gray);
+}
+
+.Card-Body .details p {
+    padding-top: .5rem;
+    padding-left: .5rem;
+}
+
+</style>
