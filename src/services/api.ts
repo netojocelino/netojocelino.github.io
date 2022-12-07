8443f0cc424e2017e422c9ac4801bf9837378fae
Neto Jocelino <netoj96@live.com>
2022-12-07 18:43:45 -0300

    fix lint at api service - adjust indent - use double quotes - add comma at end of line - add :key for v-for directive

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,61 +1,68 @@
 const URLS = {
-    main: './data.json',
-}
+  main: "./data.json",
+};
 
 const api = {
-    about: async () => {
-        const response = await fetch(URLS.main);
+  about: async () => {
+    const response = await fetch(URLS.main);
 
-        if (!response.ok) {
-            throw Error(`[${response.status}] HTTP Error!`)
-        }
-
-        const data = await response.json()
-
-        const header = {
-            title: data.name,
-            role: data.role,
-            status: data.professional.status,
-        }
-
-        const contacts = data.contact
-
-        const experiences = data.company.map((experience: any) => {
-            const getMonthString = [
-                'jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez',
-            ]
-            const startAt = new Date(experience.startAt.toString())
-            const endAt = new Date(experience.ends.toString())
-
-            const startedYearAt = startAt.getUTCFullYear()
-            const startedMonthAt = getMonthString[startAt.getUTCMonth()]
-
-            const endedYearAt = endAt.getUTCFullYear()
-            const endedMonthAt = getMonthString[endAt.getUTCMonth()]
-
-            experience.workForLong =  `${experience.location} — ${startedMonthAt} de ${startedYearAt} - ${endedMonthAt} ${endedYearAt}`
-
-            return experience
-        } )
-
-        const repoitems = data.repositories.map((repo: any) => ({
-            title: repo.name,
-            description: repo.description,
-            anchorTo: repo.html_url,
-            anchorTitle: repo.name,
-
-        }))
-
-        return {
-            header,
-            contacts,
-            experiences,
-            repoitems,
-        }
-        
-
+    if (!response.ok) {
+      throw Error(`[${response.status}] HTTP Error!`);
     }
-}
 
+    const data = await response.json();
 
-export default api
+    const header = {
+      title: data.name,
+      role: data.role,
+      status: data.professional.status,
+    };
+
+    const contacts = data.contact;
+
+    const experiences = data.company.map((experience: any) => {
+      const getMonthString = [
+        "jan",
+        "fev",
+        "mar",
+        "abr",
+        "mai",
+        "jun",
+        "jul",
+        "ago",
+        "set",
+        "out",
+        "nov",
+        "dez",
+      ];
+      const startAt = new Date(experience.startAt.toString());
+      const endAt = new Date(experience.ends.toString());
+
+      const startedYearAt = startAt.getUTCFullYear();
+      const startedMonthAt = getMonthString[startAt.getUTCMonth()];
+
+      const endedYearAt = endAt.getUTCFullYear();
+      const endedMonthAt = getMonthString[endAt.getUTCMonth()];
+
+      experience.workForLong = `${experience.location} — ${startedMonthAt} de ${startedYearAt} - ${endedMonthAt} ${endedYearAt}`;
+
+      return experience;
+    });
+
+    const repoitems = data.repositories.map((repo: any) => ({
+      title: repo.name,
+      description: repo.description,
+      anchorTo: repo.html_url,
+      anchorTitle: repo.name,
+    }));
+
+    return {
+      header,
+      contacts,
+      experiences,
+      repoitems,
+    };
+  },
+};
+
+export default api;
